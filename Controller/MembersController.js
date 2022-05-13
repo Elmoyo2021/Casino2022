a0dca9ca97c025d3f9fb2884645f360f1d878d26
Elmoyo2021 <wteamelmo@gmail.com>
2022-05-13 08:12:24 +0800

    2022-05-13 NewDay

--- Controller/MembersController.js
+++ Controller/MembersController.js
@@ -365,43 +365,4 @@ function postscriptCode(data) {
 }
 
 
-function whiteList(data) {//黑白名單列表
-    let sql = "select * from backend_whitelist  order by " + data.orderBy + " " + data.order + " limit ?,?"
-    let dataList = query(sql, [Number(data.skip), Number(data.limit)])
-    return dataList
-}
-function whiteListTotal(data) {//黑白名單總數
-    let sql = 'SELECT count(*) as total FROM `backend_whitelist`'
-    let dataList = query(sql)
-    return dataList
-}
-
-
-function whiteListInsert(data) {//寫入backend_whitelist資料庫
-    let sql = 'INSERT INTO backend_whitelist SET ?'
-    let dataList = query(sql, data)
-    return dataList
-}
-
-function whiteListUpdate(data,id) {//更新backend_whitelist資料庫
-    let sql = 'update backend_whitelist SET ? where id=?'
-    let dataList = query(sql, [data,id])
-    return dataList
-}
-function whiteListDel(id) {//刪除backend_whitelist資料庫
-    let sql = 'delete from backend_whitelist  where id=?'
-    let dataList = query(sql, [id])
-    return dataList
-}
-function whiteListInsertRt(data) {//查看backend_whitelist
-    let sql = 'SELECT * FROM `backend_whitelist` where id=?'
-    let dataList = query(sql, [data])
-    return dataList
-}
-
-function SelectListCheck(data) {//查看backend_whitelist
-    let sql = 'SELECT * FROM `backend_whitelist` where ip=?'
-    let dataList = query(sql, [data])
-    return dataList
-}
 module.exports = MembersController;
